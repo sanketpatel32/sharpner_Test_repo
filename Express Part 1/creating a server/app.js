d6ca62605d0e9169009a9957f02658eab4b0bdf8
Sanket Patel <sanpatel323@gmail.com>
2025-03-14 01:35:20 +0530

    Added custom middlewares

--- Express Part 1/creating a server/app.js
+++ Express Part 1/creating a server/app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 3000; 
+const port = 3000;
+
+// Custom middleware function to add req.user property
+const addUserProperty = (req, res, next) => {
+  req.user = 'Guest';
+  next();
+};
+
+// Apply the middleware to the /welcome route
+app.use('/welcome', addUserProperty);
+
+// Route handler for /welcome
+app.get('/welcome', (req, res) => {
+  res.send(`<h1>Welcome, ${req.user}!</h1>`);
+});
 
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}! Ready to handle requests.`);
