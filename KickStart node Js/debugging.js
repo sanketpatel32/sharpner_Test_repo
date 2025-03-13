9ec2e8076add6c3d133ea08cfe5f569cbeb48c73
Sanket Patel <sanpatel323@gmail.com>
2025-03-13 11:47:20 +0530

    Added Express server

---
+++ KickStart node Js/debugging.js
@@ -0,0 +1,19 @@
+let add = (a,b) =>{
+    return a+b;
+}
+
+let sub = (a,b) =>{
+    return a-b;
+}
+
+let mul = (a,b) =>{ 
+    return a*b;
+}   
+let div = (a,b) =>{     
+    return a/b;
+}
+
+let a = 10;
+let b = 20; 
+let c = add(a,b);
+let d = sub(a,b);
