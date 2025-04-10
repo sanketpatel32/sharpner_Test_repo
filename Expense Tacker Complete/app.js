0f3c5c7d7942c866320b8686c59f640d652c0864
Sanket Patel <sanpatel323@gmail.com>
2025-04-10 17:57:20 +0530

    Lets go live

--- Expense Tacker Complete/app.js
+++ Expense Tacker Complete/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const sequelize = require('./utils/database');
 const app = express();
-const port = 3000;
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+require('dotenv').config();
+const fs = require('fs');
+const sequelize = require('./utils/database');
 const User = require('./models/userModel');
 const Expense = require('./models/expenseModel');
-
 User.hasMany(Expense, { foreignKey: 'userId', onDelete: 'CASCADE' });
 Expense.belongsTo(User, { foreignKey: 'userId' });
 
@@ -14,17 +17,23 @@ const userRoutes = require('./routes/userRoutes');
 const expenseRoutes = require('./routes/expenseRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
 const passwordRoutes = require('./routes/passwordRoutes');
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+// app.use(helmet());
+app.use(compression());
+app.use(morgan('combined',{stream: accessLogStream}));
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
+
 
 //Routes
 app.use('/user', userRoutes);
@@ -34,8 +43,8 @@ app.use('/password',passwordRoutes)
 
 sequelize.sync({force : false}) // Set force to true only for development/testing purposes
 .then(() => {
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`);
   });
 })
 .catch(err => {
